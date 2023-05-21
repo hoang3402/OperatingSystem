db097c66a6df4fa8188e8af12a567aa57deefab5
HoangDSR <nvh.02021995@gmail.com>
2023-05-21 11:14:23 +0700

    Done OddEvenSort

---
+++ OddEvenSort.cs
@@ -0,0 +1,36 @@
+﻿namespace OperatingSystem
+{
+    internal class OddEvenSort
+    {
+        static public void sort(List<int> list)
+        {
+            int n = list.Count;
+            bool isSorted = false;
+            while (!isSorted)
+            {
+                isSorted = true;
+                int temp = 0;
+                for (int i = 1; i <= n - 2; i += 2)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+                        isSorted = false;
+                    }
+                }
+                for (int i = 0; i <= n - 2; i += 2)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+                        isSorted = false;
+                    }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using OperatingSystem;
+
+List<int> a = new List<int>()
+{
+    9,8,7,6,5,4,3,2,1
+};
+
+OddEvenSort.sort(a);
+
+foreach (int i in a)
+{
+    Console.Write(i + " ");
+}
