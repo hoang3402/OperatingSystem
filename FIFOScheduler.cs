edcd28f7c0557ce608b992409da97532fed9c211
HoangDSR <nvh.02021995@gmail.com>
2023-05-26 15:17:20 +0700

    Done FIFO

---
+++ FIFOScheduler.cs
@@ -0,0 +1,28 @@
+﻿namespace OperatingSystem
+{
+    internal class FIFOScheduler : Scheduler
+    {
+        public override Process GetNext()
+        {
+            return this.processes[0];
+        }
+
+        public override void Run()
+        {
+            Console.WriteLine("Current scheduler is FIFO");
+            while (this.processes.Count > 0)
+            {
+                Process process = GetNext();
+                process.RemainingTime--;
+                if (process.RemainingTime == 0)
+                {
+                    this.processes.RemoveAt(0);
+                    process.FinishTime = timer + 1;
+                    this.turnAroundTime += process.TurnAroundTime;
+                    this.waitingTime += process.WaitingTime;
+                }
+                timer++;
+            }
+        }
+    }
+}
---
+++ Scheduler.cs
@@ -0,0 +1,39 @@
+﻿namespace OperatingSystem
+{
+    internal abstract class Scheduler
+    {
+        protected int timer = 0;
+        protected List<Process> processes = new List<Process>();
+        protected int turnAroundTime = 0;
+        protected int waitingTime = 0;
+        protected int numberProcess = 0;
+
+        public int Timer { get => timer; }
+
+        public void AddProcess(Process process)
+        {
+            this.processes.Add(process);
+            this.numberProcess++;
+        }
+
+        public void AddProcesses(List<Process> processes)
+        {
+            this.processes.AddRange(processes);
+            this.numberProcess += processes.Count;
+        }
+
+        abstract public void Run();
+
+        abstract public Process GetNext();
+
+        public float GetAVGTurnAroundTime()
+        {
+            return this.turnAroundTime / (float)this.numberProcess;
+        }
+
+        public float GetAVGWaitingTime()
+        {
+            return this.waitingTime / (float)this.numberProcess;
+        }
+    }
+}
