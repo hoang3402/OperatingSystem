9105dfe7a500d985cfbd30f5efdc782c6ae7a832
HoangDSR <nvh.02021995@gmail.com>
2023-06-02 10:25:25 +0700

    Learn Socket

---
+++ Client.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace OperatingSystem
+{
+    internal class Client
+    {
+        static public void Main()
+        {
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2023);
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+            ProtocolType.Tcp);
+            client.Connect(iep);
+
+            NetworkStream networkStream = new NetworkStream(client);
+            StreamReader streamReader = new StreamReader(networkStream);
+            StreamWriter streamWriter = new StreamWriter(networkStream);
+            string s = streamReader.ReadLine()!;
+            Console.WriteLine("Server gui:{0}", s);
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine()!;
+                streamWriter.WriteLine(input);
+                streamWriter.Flush();
+                if (input.ToUpper().Equals("THOAT")) break;
+                s = streamReader.ReadLine()!;
+                Console.WriteLine("Server gui:{0}", s);
+            }
+            client.Disconnect(true);
+            client.Close();
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace OperatingSystem
+{
+    internal class Server
+    {
+        static public void Main()
+        {
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2023);
+            Socket _server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _server.Bind(iep);
+            _server.Listen(10);
+            Console.WriteLine($"Server is running... on port: {iep.Port}");
+            Socket client = _server.Accept();
+
+            NetworkStream networkStream = new NetworkStream(client);
+            StreamReader streamReader = new StreamReader(networkStream);
+            StreamWriter streamWriter = new StreamWriter(networkStream);
+
+            Console.WriteLine($"Client connected! Client is {client?.RemoteEndPoint?.ToString()}");
+
+            string s = "Chao ban den voi Server";
+            streamWriter.WriteLine(s);
+            streamWriter.Flush();
+            while (true)
+            {
+                s = streamReader.ReadLine()!;
+                Console.WriteLine("Client gui len:{0}", s);
+                //Neu chuoi nhan duoc la Thoat thi thoat
+                if (s.ToUpper().Equals("THOAT")) break;
+                //Gui tra lai cho client chuoi s
+                s = s.ToUpper();
+                streamWriter.WriteLine(s);
+                streamWriter.Flush();
+
+            }
+            client?.Shutdown(SocketShutdown.Both);
+            client?.Close();
+            _server.Close();
+        }
+    }
+}
